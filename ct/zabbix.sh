e017e6c6504dbd508c23b095914aaa950031a5f2
Peter Sengstock <pgsengstock@gmail.com>
2025-06-17 22:08:32 -0500

    test for lts version only

--- ct/zabbix.sh
+++ ct/zabbix.sh
@@ -38,10 +38,10 @@ function update_script() {
   cp -R /usr/share/zabbix/ /opt/zabbix-backup/
   #cp -R /usr/share/zabbix-* /opt/zabbix-backup/ Remove temporary
   rm -Rf /etc/apt/sources.list.d/zabbix.list
-  cd /tmp
+  cd /tmp || exit
   curl -fsSL "$(curl -fsSL https://repo.zabbix.com/zabbix/ |
-    grep -oP '(?<=href=")[0-9]+\.[0-9]+(?=/")' | sort -V | tail -n1 |
-    xargs -I{} echo "https://repo.zabbix.com/zabbix/{}/release/debian/pool/main/z/zabbix-release/zabbix-release_latest+debian12_all.deb")" \
+    grep -oP '(?<=href=")[0-9]+\.0+(?=/")' | sort -V | tail -n1 |
+    xargs -I{} echo "https://repo.zabbix.com/zabbix/{}/debian/pool/main/z/zabbix-release/zabbix-release_latest+debian12_all.deb")" \
     -o /tmp/zabbix-release_latest+debian12_all.deb
   $STD dpkg -i zabbix-release_latest+debian12_all.deb
   $STD apt-get update
